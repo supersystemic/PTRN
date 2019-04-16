169e6410b8180e47bad9b5a2ddb4067ac55f6fca
Marcel Goethals <marcel.goethals@gmail.com>
2019-04-16 13:27:19 +0200

    versions and basic inheritance

--- src/server/api.js
+++ src/server/api.js
@@ -15,6 +15,9 @@ app.use(function(req, res, next) {
   next()
 })
 
+const PARENT = entities.declare().id
+entities.set_value(PARENT, "parent")
+
 function check_validity(id){
     if(!entities.get(id)){
         return false
@@ -30,6 +33,20 @@ function parse_query(q){
         return {
             executed: true,
             answer: entities.declare()
+        }
+    }
+
+    if(query==="instantiate"){
+        if(!check_validity(args.id)){
+            return {executed: false, error: "unknown entity "+args.id}
+        }
+
+        let n = entities.declare()
+        relations.relate(PARENT, n.id, args.id)
+
+        return {
+            executed: true,
+            answer: n
         }
     }
 
@@ -49,10 +66,7 @@ function parse_query(q){
 
     if(query==="get"){
         if(!check_validity(args.id)){
-            return {
-                executed: false,
-                error: "unknown entity "+args.id
-            }
+            return {executed: false, error: "unknown entity "+args.id}
         }
 
         let entity = entities.get(args.id)
@@ -69,9 +83,30 @@ function parse_query(q){
             }
         })
 
+        //deal with inheritance of values
+        if(rels[PARENT]!==undefined && answer.value===undefined) {
+            answer.value = entities.get(rels[PARENT][0].bid).value
+        }
+
         return {
             executed: true,
             answer
+        }
+    }
+
+    if(query==="getversion"){
+        if(!check_validity(args.id)){
+            return {
+                executed: false,
+                error: "unknown entity "+args.id
+            }
+        }
+
+        let entity = entities.get_version(args.id, args.version)
+
+        return {
+            executed: true,
+            answer: entity
         }
     }
 
@@ -119,62 +154,3 @@ app.post("/", function(req, res) {
 var server = app.listen(3000, function () {
     console.log("app running on port.", server.address().port)
 });
-
-
-/*var http = require('http')
-
-const entities = require("./entities")
-const relations = require("./relations")
-
-function parse_query(q){
-    let query = q.query
-    let args = q.args
-
-    if(query==="declare"){
-        return entities.declare()
-    }
-
-    if(query==="set_value"){
-        return entities.set_value(args.id, args.value)
-    }
-
-    if(query==="get"){
-        return entities.get(args.id)
-    }
-
-    if(query==="relate"){
-        return entities.get(args.typeid, args.aid, args.bid)
-    }
-}
-
-http.createServer((request, response) => {
-    const { headers, method, url } = request
-    let body = []
-    request.on('error', (err) => {
-        console.error(err)
-    }).on('data', (chunk) => {
-        body.push(chunk)
-    }).on('end', () => {
-        body = Buffer.concat(body).toString()
-        let jsonbody
-        try {
-            jsonbody = JSON.parse(body)
-        } catch (e) {
-            response.statusCode = 404;
-            response.end();
-            return
-        }
-
-        response.on('error', (err) => {
-            console.error(err)
-        })
-
-        response.writeHead(200, {'Content-Type': 'application/json'})
-
-        //const responseBody = { headers, method, url, jsonbody }
-        response.write(JSON.stringify(parse_query(jsonbody)))
-
-        response.end()
-    })
-}).listen(8080)
-*/
--- src/server/entities.js
+++ src/server/entities.js
@@ -4,7 +4,8 @@ let entities = []
 //Generates an id and frees memory for this entity
 function declare() {
     let entity = {
-        id: entities.length
+        id: entities.length,
+        values: []
     }
     entities.push(entity)
     return entity
@@ -12,17 +13,32 @@ function declare() {
 
 //Sets the value for a given entity with id
 function set_value(id, value) {
-    if(!entities[id]) return undefined
+    entities[id].values.push(entities[id].value)
     entities[id].value = value
     return entities[id]
 }
 
 //Returns entity with id
 function get(id) {
-    return entities[id]
+    let e = entities[id]
+    return {
+        version: e.values.length,
+        id: e.id,
+        value: e.value
+    }
 }
+
+function get_version(id, v) {
+    let e = entities[id]
+    return {
+        version: v,
+        id: e.id,
+        value: e.values[v] || e.value
+    }
+}
+
 
 module.exports = {
     declare, set_value,
-    get
+    get, get_version
 }
